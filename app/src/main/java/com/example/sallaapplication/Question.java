2f6e51d2e6808c0120bb5ed1db87d051cba51556
Maroomalrefai <149938618+Maroomalrefai@users.noreply.github.com>
2024-03-07 21:58:26 +0300

    adding qusetion page

--- app/src/main/java/com/example/sallaapplication/Question.java
+++ app/src/main/java/com/example/sallaapplication/Question.java
@@ -2,13 +2,24 @@ package com.example.sallaapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Question extends AppCompatActivity {
-
+ Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+        next = findViewById(R.id.button5);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Question.this,QuestionTwo.class);
+                startActivity(i);
+            }
+        });
     }
 }
