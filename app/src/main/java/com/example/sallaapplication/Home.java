dc03712c1aeb0c4128baf52d2de289d0e1d3ea51
Maroomalrefai <149938618+Maroomalrefai@users.noreply.github.com>
2024-03-28 18:15:33 +0300

    clean for you in home page

--- app/src/main/java/com/example/sallaapplication/Home.java
+++ app/src/main/java/com/example/sallaapplication/Home.java
@@ -3,19 +3,40 @@ package com.example.sallaapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import androidx.appcompat.widget.Toolbar;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.adapter.RecentsAdapter;
+import com.model.RecentData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
-    private Toolbar toolbar;
+    RecyclerView recentRecycler;
+    RecentsAdapter recentsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        toolbar=findViewById(R.id.bar);
-        setSupportActionBar(toolbar);
 
+        List<RecentData> recentDataList =new ArrayList<>();
+        recentDataList.add(new RecentData("milk","Baladuna","2 JOD",R.drawable.milk));
+        recentDataList.add(new RecentData("Cheese","Alyaom","5 JOD",R.drawable.ok));
+        recentDataList.add(new RecentData("milk","Baladuna","2 JOD",R.drawable.good));
+        recentDataList.add(new RecentData("milk","Baladuna","2 JOD",R.drawable.notok));
 
+        setRecentRecycler(recentDataList);
+
+    }
+
+    private void setRecentRecycler(List<RecentData> recentsDataList) {
+        recentRecycler = findViewById(R.id.recent_Recycle);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        recentRecycler.setLayoutManager(layoutManager);
+        recentsAdapter = new RecentsAdapter(this, recentsDataList);
+        recentRecycler.setAdapter(recentsAdapter);
     }
 }
