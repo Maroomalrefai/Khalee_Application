9d91aa75398b5390745f12d2d635bb21b55cea7d
Maroomalrefai <149938618+Maroomalrefai@users.noreply.github.com>
2024-04-13 02:21:51 +0300

    History

---
+++ app/src/main/java/com/example/sallaapplication/History.java
@@ -0,0 +1,19 @@
+package com.example.sallaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+    }
+}
