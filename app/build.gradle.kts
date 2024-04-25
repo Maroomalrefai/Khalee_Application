58674c110a598b98ef075bc6fa63b8a813170acc
Shatha-Allouzi <149938646+Shatha-Allouzi@users.noreply.github.com>
2024-04-25 23:13:07 +0300

    Communities Main page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
     implementation ("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
     implementation("com.google.firebase:firebase-database:20.3.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
