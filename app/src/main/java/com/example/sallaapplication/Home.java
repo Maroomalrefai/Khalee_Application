064a44f2795b27b7d5e290610b5752a6a4b5bd6e
Yara Abu Samour <107335871+YaraSmmour@users.noreply.github.com>
2024-05-02 18:39:44 +0300

    Storage edited foe ImageToText

--- app/src/main/java/com/example/sallaapplication/Home.java
+++ app/src/main/java/com/example/sallaapplication/Home.java
@@ -3,11 +3,13 @@ package com.example.sallaapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,14 +34,16 @@ public class Home extends AppCompatActivity {
     CommunitiesAdapter communitiesAdapter;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         ImageView scan=findViewById(R.id.recycle_bin);
         Name =findViewById(R.id.name);
         profileIcon = findViewById(R.id.profileIcon);
+
 
         // Set profile image and user name if available
         if (user != null) {
