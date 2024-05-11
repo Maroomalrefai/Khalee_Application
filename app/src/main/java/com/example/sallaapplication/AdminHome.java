6f2bb96e924b1aca92a4c1a3202226e3d4209fdd
Maroomalrefai <149938618+Maroomalrefai@users.noreply.github.com>
2024-05-11 19:55:43 +0300

    Admin

--- app/src/main/java/com/example/sallaapplication/AdminHome.java
+++ app/src/main/java/com/example/sallaapplication/AdminHome.java
@@ -1,14 +1,14 @@
-package com.example.adminofkhalee;
+package com.example.sallaapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AdminHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_admin_home);
     }
 }
